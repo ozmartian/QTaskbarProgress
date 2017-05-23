fa10962b7cbea02471b3fbce86eba855ec860f2f
Pete Alexandrou <pete@ozmartians.com>
2017-05-23 12:31:06 +0800

    cleanup

--- qtaskbarprogress.py
+++ qtaskbarprogress.py
@@ -3,7 +3,8 @@
 
 import sys
 
-from PyQt5.QtCore import pyqtSignal, pyqtSlot, QDir, QFile, QStandardPaths, QUuid
+from PyQt5.QtCore import pyqtSignal, pyqtSlot, QDir, QFile, QProcess, QProcessEnvironment, QStandardPaths, QUuid
+from PyQt5.QtGui import QCloseEvent
 from PyQt5.QtDBus import QDBusConnection, QDBusMessage
 from PyQt5.QtWidgets import QApplication, QWidget
 
@@ -17,21 +18,54 @@ class QTaskbarProgress(QWidget):
 
     _desktopFile = QFile()
 
-    msgPath = '/com/ozmartians/QTaskbarProgress'
+    msgPath = '/'
     msgInterface = 'com.canonical.Unity.LauncherEntry'
     msgName = 'Update'
 
-    def __init__(self):
-        super(QTaskbarProgress, self).__init__()
+    errorTxt = 'QTaskbarProgress only works with KDE and Unity desktops on Linux!'
+
+    def __init__(self, parent=None):
+        super(QTaskbarProgress, self).__init__(parent)
         if not sys.platform.startswith('linux'):
-            self.errorOccurred.emit('QTaskbarProgress only works with KDE and Unity desktops on Linux!')
-            print('QTaskbarProgress only works with KDE and Unity desktops on Linux!')
+            self.errorOccurred.emit(QTaskbarProgress.errorTxt)
+            sys.stderr.write(QTaskbarProgress.errorTxt)
             return
-        QTaskbarProgress._initDesktopFile()
+        if self.checkEnvironment() and self.checkDBus():
+            self._initDesktopFile()
+        else:
+            self.errorOccurred.emit(QTaskbarProgress.errorTxt)
+            sys.stderr.write(QTaskbarProgress.errorTxt)
+            return
 
     def __del__(self):
         if self._desktopFile.exists():
             self._desktopFile.remove()
+
+    def checkEnvironment(self):
+        proc = QProcess(self)
+        proc.setProcessEnvironment(QProcessEnvironment.systemEnvironment())
+        proc.setWorkingDirectory(QDir.homePath())
+        if hasattr(proc, 'errorOccurred'):
+            self.errorOccurred.emit(proc.errorString())
+            sys.stderr.write(proc.errorString())
+            return False
+        if proc.state() == QProcess.NotRunning:
+            proc.setProcessChannelMode(QProcess.MergedChannels)
+            results = {'unity': None, 'ksmserver': None}
+            for de in ('unity', 'ksmserver'):
+                proc.start('pidof', [de])
+                proc.waitForFinished(-1)
+                results[de] = str(proc.readAllStandardOutput().data(), 'utf-8').replace('\n', '')
+            for key in results:
+                if results.get(key).isdigit():
+                    return True
+        return False
+
+    def checkDBus(self):
+        if not QDBusConnection.sessionBus().isConnected():
+            sys.stderr.write("Cannot connect to the D-Bus session bus.\n"
+                             "To start it, run:\n"
+                             "\teval `dbus-launch --auto-syntax`\n");
 
     @pyqtSlot(bool)
     def setProgressVisible(self, visible: bool):
@@ -53,40 +87,40 @@ class QTaskbarProgress(QWidget):
         QTaskbarProgress._sendMessageAttribute('count', counter)
         self.counterChanged.emit(counter)
 
-    @staticmethod
-    def _initDesktopFile():
+    def desktopFilename(self):
+        return self._desktopFile.fileName()
+
+    def _initDesktopFile(self):
         name = '%s.desktop' % QUuid.createUuid().toString()
         appDir = QDir(QStandardPaths.writableLocation(QStandardPaths.ApplicationsLocation))
-        QTaskbarProgress._desktopFile = QFile(appDir.absoluteFilePath(name))
-        if not QTaskbarProgress._desktopFile.exists():
-            QTaskbarProgress._desktopFile.open(QFile.WriteOnly)
-            QTaskbarProgress._desktopFile.write(b'[Desktop Entry]\n')
-            QTaskbarProgress._desktopFile.write(b'Type=Application\n')
-            QTaskbarProgress._desktopFile.write(b'Version=1.1\n')
-            QTaskbarProgress._desktopFile.write(b'Name=' + QApplication.applicationName().encode() + b'\n')
-            QTaskbarProgress._desktopFile.write(b'Exec=' + QApplication.applicationFilePath().encode() + b'\n')
-            QTaskbarProgress._desktopFile.close()
-        QTaskbarProgress._sendReset()
+        self._desktopFile = QFile(appDir.absoluteFilePath(name))
+        if not self._desktopFile.exists():
+            self._desktopFile.open(QFile.WriteOnly)
+            self._desktopFile.write(b'[Desktop Entry]\n')
+            self._desktopFile.write(b'Type=Application\n')
+            self._desktopFile.write(b'Version=1.1\n')
+            self._desktopFile.write(b'Name=' + QApplication.applicationName().encode() + b'\n')
+            self._desktopFile.write(b'Exec=' + QApplication.applicationFilePath().encode() + b'\n')
+            self._desktopFile.close()
+        self._sendReset()
 
-    @staticmethod
-    def _sendReset():
+    def _sendReset(self):
         props = {
             'progress-visible': True,
             'progress': 0.0,
             'count-visible': False,
             'count': 0
         }
-        QTaskbarProgress._sendMessage(props)
+        self._sendMessage(props)
 
-    @staticmethod
-    def _sendMessage(params: dict):
-        if not QTaskbarProgress._desktopFile.exists():
-            QTaskbarProgress._initDesktopFile()
+    def _sendMessage(self, params: dict):
+        if not self._desktopFile.exists():
+            self._initDesktopFile()
         message = QDBusMessage.createSignal(QTaskbarProgress.msgPath, QTaskbarProgress.msgInterface,
                                             QTaskbarProgress.msgName)
-        message << 'application://%s' % QTaskbarProgress._desktopFile << params
+        message << 'application://%s' % self._desktopFile << params
+        print(message)
         QDBusConnection.sessionBus().send(message)
 
-    @staticmethod
-    def _sendMessageAttribute(key: str, value: any):
-        QTaskbarProgress._sendMessage({key: value})
+    def _sendMessageAttribute(self, key: str, value: any):
+        self._sendMessage({key: value})
--- test.py
+++ test.py
@@ -3,13 +3,69 @@
 
 import sys
 
-from PyQt5.QtWidgets import QApplication
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 
 from qtaskbarprogress import QTaskbarProgress
 
 
-app = QApplication(sys.argv)
-app.setApplicationName('QTaskbarProgress Demo')
-taskbar = QTaskbarProgress()
-taskbar.setProgressVisible(True)
-taskbar.setProgress(50.0)
+class Widget(QWidget):
+    def __init__(self, parent=None):
+        super(Widget, self).__init__(parent)
+
+        self.taskbar = QTaskbarProgress(self)
+
+        self.progressCheckBox = QCheckBox(self)
+        self.progressCheckBox.setText('Progress Bar:')
+        self.progressCheckBox.setCursor(Qt.PointingHandCursor)
+        self.progressSlider = QSlider(Qt.Horizontal, self)
+        self.progressSlider.setRange(0, 100)
+        self.progressSlider.setCursor(Qt.PointingHandCursor)
+
+        self.counterCheckBox = QCheckBox(self)
+        self.counterCheckBox.setText('Progress Counter:')
+        self.counterCheckBox.setCursor(Qt.PointingHandCursor)
+        self.counterSpinBox = QSpinBox(self)
+        self.counterSpinBox.setWrapping(True)
+        self.counterSpinBox.setButtonSymbols(QAbstractSpinBox.PlusMinus)
+        self.counterSpinBox.setAccelerated(True)
+        self.counterSpinBox.setCursor(Qt.PointingHandCursor)
+
+        desktopFileLabel = QLabel('Desktop File:', self)
+        self.desktopFileInput = QLineEdit(self)
+        self.desktopFileInput.setReadOnly(True)
+        self.desktopFileInput.setText(self.taskbar.desktopFilename())
+
+        desktopFileLayout = QVBoxLayout()
+        desktopFileLayout.setSpacing(5)
+        desktopFileLayout.addWidget(desktopFileLabel)
+        desktopFileLayout.addWidget(self.desktopFileInput)
+
+        layout = QFormLayout()
+        layout.setSpacing(20)
+        layout.setLabelAlignment(Qt.AlignLeft)
+        layout.addRow(self.progressCheckBox, self.progressSlider)
+        layout.addRow(self.counterCheckBox, self.counterSpinBox)
+        layout.addRow(desktopFileLayout)
+
+        self.progressCheckBox.clicked.connect(self.taskbar.setProgressVisible)
+        self.counterCheckBox.clicked.connect(self.taskbar.setCounterVisible)
+        self.counterSpinBox.valueChanged.connect(self.taskbar.setCounter)
+        self.progressSlider.valueChanged.connect(self.taskbar.setProgress)
+        self.taskbar.errorOccurred.connect(lambda error: QMessageBox.critical(self, 'Error', error))
+
+        self.setLayout(layout)
+        self.setWindowIcon(self.style().standardIcon(QStyle.SP_TitleBarMenuButton))
+        self.setMinimumWidth(600)
+
+
+def main():
+    app = QApplication(sys.argv)
+    app.setApplicationName('QTaskbarProgressDemo')
+    widget = Widget()
+    widget.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
